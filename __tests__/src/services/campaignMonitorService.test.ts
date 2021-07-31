0c0a5128d670e5f6afbc17b5af4e5a6f4bb5d6f8
Richard Chou <konami99@hotmail.com>
2021-07-31 13:13:20 +1000

    temp

--- __tests__/src/services/campaignMonitorService.test.ts
+++ __tests__/src/services/campaignMonitorService.test.ts
@@ -1,6 +1,10 @@
 import { DateTime } from "luxon";
 import Campaign from '../../../src/models/campaign';
 import campaignMonitorService from '../../../src/services/campaignMonitorService';
+import axios from 'axios';
+import ssmService from '../../../src/services/SsmService';
+
+jest.mock('axios');
 
 describe('campaignFilter', () => {
   const campaigns: Campaign[] = [
@@ -113,5 +117,97 @@ describe('sortCampaigns', () => {
         dateScheduledObject: DateTime.fromISO('2021-07-25T13:00:00')
       }
     ]);
+  });
+});
+
+describe('getClients', () => {
+  it('gets clients', async () => {
+    ssmService.getParameter = jest.fn().mockResolvedValue('key12345');
+    axios.create = jest.fn().mockReturnValue({
+      get: jest.fn().mockResolvedValue({
+        data: 'data123'
+      }),
+    });
+    
+    expect(await campaignMonitorService.getClients()).toEqual('data123');
+  });
+});
+
+describe('getScheduledCampaigns', () => {
+  it('gets scheduled campaigns', async () => {
+    const clientID = 'id123';
+    ssmService.getParameter = jest.fn().mockResolvedValue('key12345');
+    axios.create = jest.fn().mockReturnValue({
+      get: jest.fn().mockResolvedValue({
+        data: 'data456'
+      }),
+    });
+    
+    expect(await campaignMonitorService.getScheduledCampaigns(clientID)).toEqual('data456');
+  });
+});
+
+describe('getCampaignDetails', () => {
+  it('gets scheduled campaigns', async () => {
+    const campaignID = 'id123';
+    ssmService.getParameter = jest.fn().mockResolvedValue('key12345');
+    axios.create = jest.fn().mockReturnValue({
+      get: jest.fn().mockResolvedValue({
+        data: 'data000'
+      }),
+    });
+    
+    expect(await campaignMonitorService.getCampaignDetails(campaignID)).toEqual('data000');
+  });
+});
+
+describe('serializeCampaigns', () => {
+  it('gets scheduled campaigns', async () => {
+    const campaigns: Campaign[] = [
+      {
+        clientName: 'client1',
+        dateScheduled: 'date1',
+        scheduledTimeZone: 'tz1',
+        campaignID: 'id1',
+        name: 'name1',
+        numberOfRecipients: 23000,
+        localDateScheduledObject: DateTime.fromISO('2021-07-25T13:00:00')
+      },
+      {
+        clientName: 'client2',
+        dateScheduled: 'date2',
+        scheduledTimeZone: 'tz2',
+        campaignID: 'id2',
+        name: 'name2',
+        numberOfRecipients: 1000,
+        localDateScheduledObject: DateTime.fromISO('2021-07-26T13:00:00')
+      },
+      {
+        clientName: 'client3',
+        dateScheduled: 'date3',
+        scheduledTimeZone: 'tz3',
+        campaignID: 'id3',
+        name: 'name3',
+        numberOfRecipients: 1001,
+        localDateScheduledObject: DateTime.fromISO('2021-07-27T13:00:00')
+      }
+    ];
+
+    const expectedResult = "\
+client1\n\
+2021-07-25T13:00:00.000+10:00\n\
+name1\n\
+23000\n\n\
+client2\n\
+2021-07-26T13:00:00.000+10:00\n\
+name2\n\
+1000\n\n\
+client3\n\
+2021-07-27T13:00:00.000+10:00\n\
+name3\n\
+1001\n\n\
+";
+
+    expect(await campaignMonitorService.serializeCampaigns(campaigns)).toEqual(expectedResult);
   });
 });
